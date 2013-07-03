1d221fabe5c3133ffa4ab7c9de8be9b8f84afa26
gglin <george.g.lin@gmail.com>
2013-07-03 12:32:00 -0400

    First commit - test git create

---
+++ db/migrate/20130702212804_create_posts.rb
@@ -0,0 +1,16 @@
+class CreatePosts < ActiveRecord::Migration
+  def up
+    create_table :posts do |t|
+      t.string :title
+      t.text :body
+      t.timestamps
+    end
+    Post.create(title: "My Octopress Blog", body: "Hello World!")
+    Post.create(title: "How to lasso your dog", body: "1. Tie a rope into a lasso. 2. Swing it over that unruly dog's torso. 3. Gently pull.")
+    Post.create(title: "Top 10 coffee shops in Austin", body: "1..10: Epoch Coffee, the 24/7 coffee shop.")
+  end
+
+  def down
+    drop_table :posts
+  end
+end
